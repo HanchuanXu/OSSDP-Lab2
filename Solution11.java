8ae0d1c672344e5fdde64f89af42c9fe461f9b65
2021110906 <127077642+2021110906@users.noreply.github.com>
2023-11-04 13:16:13 +0800

    Add files via upload

--- Solution11.java
+++ Solution11.java
@@ -28,44 +28,42 @@ import java.util.Arrays;
  * 输出：[[0,0,0]]
  * 解释：唯一可能的三元组和为 0 。
  */
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length();
+public class Solution11 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        // 枚举 a
+        List<List<Integer>> ans = new ArrayList<>();
+
         for (int first = 0; first < n; ++first) {
-            // 需要和上一次枚举的数不相同
             if (first > 0 && nums[first] == nums[first - 1]) {
                 continue;
             }
-            // c 对应的指针初始指向数组的最右端
+
             int third = n - 1;
-            int target = -nums[first]
-            // 枚举 b
+            int target = -nums[first];
+
             for (int second = first + 1; second < n; ++second) {
-                // 需要和上一次枚举的数不相同
                 if (second > first + 1 && nums[second] == nums[second - 1]) {
                     continue;
                 }
-                // 需要保证 b 的指针在 c 的指针的左侧
-                while (second < third & nums[second] + nums[third] > target) {
+
+                while (second < third && nums[second] + nums[third] > target) {
                     --third;
                 }
-                // 如果指针重合，随着 b 后续的增加
-                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
-                if (second === third) {
-                    break;
-                }
-                if (nums[second] + nums[third] == target) {
-                    List<Integer> list = new ArrayList<Integer>();
+
+                if (second < third && nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
                     list.add(nums[first]);
                     list.add(nums[second]);
                     list.add(nums[third]);
-                    ans.add(list);
+
+                    if (!ans.contains(list)) {
+                        ans.add(list);
+                    }
                 }
             }
         }
+
         return ans;
     }
 }
