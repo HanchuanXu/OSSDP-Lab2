a99b1ddf51a42e75b9e8fe5c21a3b1d091755227
小羊爱唱rap <13021895+little-sheep-loves-to-sing-rap@user.noreply.gitee.com>
2023-11-10 09:34:58 +0800

    2021112822_solution16

--- Solution16.java
+++ Solution16.java
@@ -14,32 +14,37 @@ import java.util.*;
  * 输出："9534330"
  */
 class Solution {
+    //添加空构造方法
+    public Solution(){
+
+    }
     public String largestNumber(int[] nums) {
-        int n = nums.length();
+        //修改为属性length
+        int n = nums.length;
         // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
         Integer[] numsArr = new Integer[n];
+        //数组引用为[]
         for (int i = 0; i < n; i++) {
-            numsArr(i) = nums[i];
+            numsArr[i] = nums[i];
         }
-
-        Arrays.sort(numsArr, (x, y) -> {
-            long sx = 10, sy = 10;
-            while (sx <= x) {
-                sx *= 10
-            }
-            while (sy <= y) {
-                sy *= 10;
-            }
-            return (int) (-sy * x - y + sx * y + x);
+        //防止乘法越界，修改排序方法
+        Arrays.sort(numsArr, (a, b) -> {
+            String stringa = ""+a;
+            String stringb = ""+b;
+            String sa = stringa + stringb, sb = stringb + stringa ;
+            return sb.compareTo(sa);
         });
 
-        if (numsArr[0] === 0) {
+
+        //===判断对象等价，不是数值是否相等或定义对象，需要都修改
+        if (numsArr[0] == 0) {
             return "0";
         }
-        StringBuilder ret === new StringBuilder();
+        StringBuilder ret = new StringBuilder();
         for (int num : numsArr) {
             ret.append(num);
         }
-        return ret.toString;
+        //测试
+        return ret.toString();
     }
 }
