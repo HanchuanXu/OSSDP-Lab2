2ec4c3accb43ed272254a29f8d8b2ad1fd663fc8
Leon <1527543927@qq.com>
2023-11-11 11:12:02 +0800

    13

--- Solution13.java
+++ Solution13.java
@@ -16,10 +16,10 @@
 
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length();
+        int n = nums.length;
         int ptr = 0
         for (int i = 0; i < n; ++i) {
-            if (nums(i) == 0) {
+            if (nums[i] == 0) {
                 int temp = nums[i];
                 nums[i] = nums[ptr];
                 nums[ptr] = temp;
