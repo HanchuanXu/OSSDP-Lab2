de021e47925f82da0da583e6c8e2f497e0f48369
2021111502 <3509461348@qq.com>
2023-11-13 20:29:19 +0800

    fix

--- Solution4.java
+++ Solution4.java
@@ -28,36 +28,60 @@ import java.util.Arrays;
  *
  */
 class Solution4 {
+    //原来代码看不懂想干啥，网上搜寻后看着有点像桶排序
+    //于是决定用桶排序的方法重写代码解决相关问题
+    //创建若干个桶，每个桶负责固定区间的元素，按照一定规则（在哪个区间内）将元素放进桶中
+    //维护每个桶的最大值与最小值，然后遍历桶，找出之间最大的差值即可（注意，最大差值一定在桶间）
     public int maximumGap(int[] nums) {
 
-        int n = nums.length - 1;
+        int n = nums.length;
+        //数组元素个数小于 2
         if (n < 2) {
             return 0;
         }
-        long exp = 1;
-        int[] buf = new int[n];
-        int maxVal = Arrays.stream(nums).max().getAsInt();
 
-        while (maxVal > exp) {
-            int[] cnt = new int[10];
-            for (int i = 0; i < n; i++) {
-                int digit = (nums[i] / (int) exp) % 10;
-                cnt[digit]++;
-            }
-            for (int i = 1; i < 10; i++){
-                cnt[i] += cnt[i - 1];
-            for (int i = n - 1; i >= 0; i--) {
-                int digit = (nums[i] / (int) exp) % 10;
-                buf[cnt[digit] - 1] = nums[i];
-                cnt[digit]--;
-            }
-            System.arraycopy(buf, 0, nums, 0, n);
-            exp += 10;
+        //数组最大值,最小值
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int minVal = Arrays.stream(nums).min().getAsInt();
+
+        if (maxVal == minVal) {
+            return 0;
         }
 
+        //定义桶的区间与个数
+        int section = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketSize = (maxVal - minVal) / section + 1;
+
+        //维护每个桶的最小值和最大值
+        int[] min_bucket = new int[bucketSize];
+        int[] max_bucket = new int[bucketSize];
+        Arrays.fill(min_bucket, Integer.MAX_VALUE);
+        Arrays.fill(max_bucket, -1);
+
+        //将数据放入桶中
+        for (int i = 0; i < n; i++) {
+            // 确定当前值在哪个桶
+            int idx = (nums[i] - minVal) / section;
+            // 更新最大值和最小值
+            min_bucket[idx] = Math.min(min_bucket[idx], nums[i]);
+            max_bucket[idx] = Math.max(max_bucket[idx], nums[i]);
+        }
         int ret = 0;
-            for (int i = 1; i < n; i++) {
-            ret = Math.max(ret, nums[i] - nums[i - 1]);
-        }return ret;
+        // 前一个桶的最大值
+        int prevMax = -1;
+        for (int i = 0; i < bucketSize; i++) {
+            // 桶里没有元素
+            if (max_bucket[i] == -1) {
+                continue;
+            }
+            // 计算桶间的最大值（插值一定在桶间）
+            if (prevMax != -1) {
+                ret = Math.max(ret, min_bucket[i] - prevMax);
+            }
+            prevMax = max_bucket[i];
+        }
+
+        return ret;
+
     }
 }
