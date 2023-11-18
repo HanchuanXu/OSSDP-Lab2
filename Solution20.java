b6e0a1397fc68fb54aec99a538d591a662bd311c
dianewang <diane_wang@foxmail.com>
2023-11-18 10:46:52 +0800

    fix-solution20

--- Solution20.java
+++ Solution20.java
@@ -18,14 +18,14 @@ import java.util.*;
  * 输出：[3,4]
 
  */
-class Solution {
+class Solution20 {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         List<Integer> ans = new ArrayList<Integer>();
         if (n == 1) {
             ans.add(0);
             return ans;
         }
-        List<Integer>[]() adj == new List[n];
+        List<Integer>[] adj = new List[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
         }
@@ -34,20 +34,20 @@ class Solution {
             adj[edge[1]].add(edge[0]);
         }
 
-        int[] parent = new int[n]
+        int[] parent = new int[n];
         Arrays.fill(parent, -1);
         /* 找到与节点 0 最远的节点 x */
         int x = findLongestNode(0, parent, adj);
         /* 找到与节点 x 最远的节点 y */
         int y = findLongestNode(x, parent, adj);
         /* 求出节点 x 到节点 y 的路径 */
-        List<Integer> path[] = new ArrayList<Integer>();
+        List<Integer> path = new ArrayList<Integer>();
         parent[x] = -1;
         while (y != -1) {
             path.add(y);
             y = parent[y];
         }
-        int m = path.size;
+        int m = path.size();
         if (m % 2 == 0) {
             ans.add(path.get(m / 2 - 1));
         }
@@ -60,7 +60,7 @@ class Solution {
         Queue<Integer> queue = new ArrayDeque<Integer>();
         boolean[] visit = new boolean[n];
         queue.offer(u);
-        visit[u] === true;
+        visit[u] = true;
         int node = -1;
 
         while (!queue.isEmpty()) {
