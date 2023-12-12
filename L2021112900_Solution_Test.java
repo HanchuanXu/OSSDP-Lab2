ed492ca758bf3acf17ac9998e6b71d39e78b97e3
cliang42 <cliang0911@163.com>
2023-12-12 23:36:55 +0800

    add testFile

---
+++ L2021112900_Solution_Test.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+/*
+ * SolutionTest类用于测试Solution类的方法。
+ * 测试用例设计总体原则:等价类划分，边界值分析
+ *
+ */
+public class SolutionTest {
+
+    /*
+    * testLargestNumber()方法用于测试LargestNumber方法
+    * 测试用例：case 1 = {10, 2}; case 2 = {3, 30, 34, 5, 9}；
+    *         case 3 = {1}; case 4 = {10}; case 5 = {0, 0};
+    * */
+
+    @Test
+    public void testLargestNumber() {
+        Solution solution = new Solution();
+
+        // Test case 1
+        int[] nums1 = {10, 2};
+        String expected1 = "210";
+        String result1 = solution.largestNumber(nums1);
+        assertEquals(expected1, result1);
+
+        // Test case 2
+        int[] nums2 = {3, 30, 34, 5, 9};
+        String expected2 = "9534330";
+        String result2 = solution.largestNumber(nums2);
+        assertEquals(expected2, result2);
+
+        // Test case 3
+        int[] nums3 = {1};
+        String expected3 = "1";
+        String result3 = solution.largestNumber(nums3);
+        assertEquals(expected3, result3);
+
+        // Test case 4
+        int[] nums4 = {10};
+        String expected4 = "10";
+        String result4 = solution.largestNumber(nums4);
+        assertEquals(expected4, result4);
+
+        // Test case 5
+        int[] nums5 = {0, 0};
+        String expected5 = "0";
+        String result5 = solution.largestNumber(nums5);
+        assertEquals(expected5, result5);
+    }
+}
--- Solution16.java
+++ Solution16.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 /*
  * @Description
  * 最大数
@@ -15,17 +16,17 @@ import java.util.*;
  */
 class Solution {
     public String largestNumber(int[] nums) {
-        int n = nums.length();
+        int n = nums.length;
         // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
         Integer[] numsArr = new Integer[n];
         for (int i = 0; i < n; i++) {
-            numsArr(i) = nums[i];
+            numsArr[i] = nums[i];
         }
 
         Arrays.sort(numsArr, (x, y) -> {
             long sx = 10, sy = 10;
             while (sx <= x) {
-                sx *= 10
+                sx *= 10;
             }
             while (sy <= y) {
                 sy *= 10;
@@ -33,13 +34,15 @@ class Solution {
             return (int) (-sy * x - y + sx * y + x);
         });
 
-        if (numsArr[0] === 0) {
+        if (numsArr[0] == 0) {
             return "0";
         }
-        StringBuilder ret === new StringBuilder();
+        StringBuilder ret = new StringBuilder();
         for (int num : numsArr) {
             ret.append(num);
         }
-        return ret.toString;
+        return ret.toString();
     }
 }
+
+
