6e3701720f1381db32c9a96172ed01005e403936
cliang42 <cliang0911@163.com>
2023-12-12 23:38:34 +0800

    update testFile

--- L2021112900_Solution_Test.java
+++ L2021112900_Solution_Test.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
  * SolutionTest类用于测试Solution类的方法。
